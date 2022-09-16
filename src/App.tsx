aa811bf034cd91a04b055030d865581c5c4f1fce
Prg-maker <df3296709@gmail.com>
2022-09-16 15:40:33 -0300

    add all

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline bg-zinc-900">
-      Hello world!
-    </h1>
-  )
+    <div className="max-full h-screen bg-white ">
+      <header className="w-full h-1/3 bg-zinc-800 flex flex-col  justify-center items-center relative">
+        <h1 className="font-sans font-semibold  text-4xl text-white">Desenhe Onde Você Quiser</h1>
+        <div className="w-[600px] h-[278px]  bg-zinc-50 rounded-lg absolute mt-[450px] shadow-2xl cursor-pointer hover:animate-pulse">
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
