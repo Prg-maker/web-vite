46cff2d8927e2eabfca1af9f92a7409aaa69b06f
Prg-maker <df3296709@gmail.com>
2022-09-16 21:29:52 -0300

    add style card download

--- src/components/CardDownload.tsx
+++ src/components/CardDownload.tsx
@@ -1,7 +1,14 @@
+import {DownloadSimple} from 'phosphor-react'
+
 export function CardDownload(){
   return(
-    <div className="w-[276px] h-[248px] bg-white shadow-cardShadow">
+    <div className="w-[276px] h-[248px] bg-white shadow-cardShadow flex flex-col items-center gap-[33px]">
+      <p className='mt-5 font-sans font-medium'>Versão Linux</p>
 
+      <DownloadSimple weight='bold' className='cursor-pointer' size={46}/>
+      
+
+      <span className='font-normal text-sm mt-7'>Repositório: <a href="" className='underline'>Vigilant Garbanzo</a>.</span>
     </div>
   )
 }
